ceda89d105683025cf72225dcc35a9811e58b0f4
NotKatsu <katsudiscord@outlook.com>
2023-02-26 10:10:21 +0000

    Made code easier to read.

--- nft-preview-component/src/pages/index.tsx
+++ nft-preview-component/src/pages/index.tsx
@@ -6,27 +6,63 @@ export default function Home() {
   return (
     <>
       <div style={{ minHeight: "100vh", display: "flex", justifyContent: "center", alignItems: "center" }}>
-        <Card css={{ width: "350px", bg: "#15273F" }}>
+        <Card 
+        css={{ width: "350px", bg: "#15273F" }}>
+
           <Spacer y={1.2} />
-          <Image width={300} src="https://cdn.discordapp.com/attachments/1074339253246496779/1079159125528817715/image-equilibrium.jpg" css={{ br: "8px" }} />
+
+          <Image 
+          width={300} 
+          src="https://cdn.discordapp.com/attachments/1074339253246496779/1079159125528817715/image-equilibrium.jpg" 
+          css={{ br: "8px" }} />
+
           <Spacer y={0.65} />
 
-          <Text css={{ fontSize: "23px", "marginLeft": "26px" }}>Equailibrium #3429</Text>
+          <Text 
+          css={{ fontSize: "23px", "marginLeft": "26px" }}
+          >Equailibrium #3429</Text>
+
           <Spacer y={0.2} />
-          <Text css={{ fontSize: "18px", "marginLeft": "26px", color: "#7186A2" }}>Our Equailibrium collection promotes</Text>
-          <Text css={{ fontSize: "18px", "marginLeft": "26px", color: "#7186A2" }}>balance and calm.</Text>
+          <Text 
+          css={{ fontSize: "18px", "marginLeft": "26px", color: "#7186A2" }}
+          >Our Equailibrium collection promotes</Text>
+
+          <Text 
+          css={{ fontSize: "18px", "marginLeft": "26px", color: "#7186A2" }}
+          >balance and calm.</Text>
           <Spacer y={1} />
           <Row>
-            <Text css={{ fontSize: "18px", "marginLeft": "26px", color: "#26FFFF" }}><SiEthereum size={16}/>  0.041 ETH</Text>
-            <Text css={{ fontSize: "18px", "marginLeft": "30%", color: "#7186A2"}}><AiFillClockCircle size={16}/>  3 days left</Text>
+            <Text 
+            css={{ fontSize: "18px", "marginLeft": "26px", color: "#26FFFF" }}
+            ><SiEthereum size={16}/>  0.041 ETH</Text>
+            <Text 
+            css={{ fontSize: "18px", "marginLeft": "30%", color: "#7186A2"}}
+            ><AiFillClockCircle size={16}/>  3 days left</Text>
           </Row>
+
           <Spacer y={1} />
-          <Divider  css={{ width: "85%", "marginLeft": "auto", "marginRight": "auto" }} />
+
+          <Divider 
+          css={{ width: "85%", "marginLeft": "auto", "marginRight": "auto" }} />
+
           <Spacer y={1} />
+
           <Row>
-            <User src="https://cdn.discordapp.com/attachments/1074339253246496779/1079167047612764280/image-avatar.png" name="" bordered color="primary" size="md" css={{ marginLeft: "20px", "marginRight": "0" }} />
-            <Text css={{ fontSize: "18px", color: "#7186A2" }}>Creation of</Text>
-            <Text css={{ fontSize: "18px", color: "#FFFFFF", "marginLeft": "4px" }}>Jules Wyvern</Text>
+            <User 
+            src="https://cdn.discordapp.com/attachments/1074339253246496779/1079167047612764280/image-avatar.png" 
+            name="" 
+            bordered 
+            color="primary" 
+            size="md" 
+            css={{ marginLeft: "20px", "marginRight": "0" }} />
+
+            <Text 
+            css={{ fontSize: "18px", color: "#7186A2" }}
+            >Creation of</Text>
+            <Text 
+            css={{ fontSize: "18px", color: "#FFFFFF", "marginLeft": "4px" }}
+            >Jules Wyvern</Text>
+
           </Row>
           <Spacer y={1.5} />
         </Card>
